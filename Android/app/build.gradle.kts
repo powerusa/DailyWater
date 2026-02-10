b2a9cf2a741e7182a648b46c890e46001fa48381
Pawel Palka <powersatellite@yahoo.com>
2026-02-09 19:52:12 -0600

    Update targetSdk and compileSdk to API 35

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.watercamel"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.watercamel"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
     }
